f0b21aa3a7434e90027eca071fc9c407c00d2f2d
DERPZ774 <86765223+DERPZ774@users.noreply.github.com>
2022-12-28 17:42:53 -0500

    Fixed redhood bug
    Fixed Red Hood grapple appearing weird on his body

--- assets/dhhp/renderers/heroes/red_hood.js
+++ assets/dhhp/renderers/heroes/red_hood.js
@@ -18,10 +18,6 @@ function initEffects(renderer) {
     utils.addLivery(renderer, "DESERT_EAGLE", "gun");
 
     renderer.bindProperty("fiskheroes:equipped_item").setItems([
-        { "anchor": "body", "scale": 0.535, "offset": [-3.05, 0.52, 3.0], "rotation": [-148.0, 90.0, 0.0] },
-        { "anchor": "body", "scale": 0.535, "offset": [3.05, 0.52, 3.0], "rotation": [-148.0, -90.0, 0.0] }
-    ]).slotIndex = 0;
-    renderer.bindProperty("fiskheroes:equipped_item").setItems([
         { "anchor": "rightLeg", "scale": 0.7, "offset": [-2.4, 0.5, 1.25], "rotation": [90.0, 0.0, 0.0] },
         { "anchor": "leftLeg", "scale": 0.7, "offset": [2.4, 0.5, 1.25], "rotation": [90.0, 0.0, 0.0] }
     ]).slotIndex = 1;
