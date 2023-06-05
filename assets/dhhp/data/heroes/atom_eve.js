1a97824e49c7fbe70d4a6fd7e5fc89bc0cc9633f
DERPZ774 <86765223+DERPZ774@users.noreply.github.com>
2023-06-05 12:13:01 -0400

    atom eve data file setup

--- assets/dhhp/data/heroes/atom_eve.js
+++ assets/dhhp/data/heroes/atom_eve.js
@@ -1,3 +1,5 @@
+var utils = implement("dhhp:external/utils");
+
 function init(hero) {
     hero.setName("Atom Eve");
     hero.setVersion("Invincible");
@@ -10,4 +12,22 @@ function init(hero) {
 
     hero.addPowers("dhhp:atom_eve");
     hero.addAttribute("PUNCH_DAMAGE", 8.0, 0);
+    hero.addAttribute("FALL_RESISTANCE", 1.0, 1);
+    hero.addAttribute("JUMP_HEIGHT", 2.0, 0);
+    hero.addAttribute("SPRINT_SPEED", 0.25, 1);
+    hero.addAttribute("IMPACT_DAMAGE", 0.25, 1);
+
+    hero.setModifierEnabled(isModifierEnabled);
+    hero.setTickHandler((entity, manager) => {
+        utils.all_tick(entity, manager, "dhhp:hero.landing", 1000)
+    });
+}
+
+function isModifierEnabled(entity, modifier) {
+    switch (modifier.name()) {
+        case "fiskheroes:regeneration":
+            return entity.getHealth() < 3;
+        default:
+            return utils.flight_auto_modifier(entity, modifier, -10);
+    }
 }
