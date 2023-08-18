1c18035e5ae5f803fbf386c6728838dd58d6996c
DERPZ774 <86765223+DERPZ774@users.noreply.github.com>
2023-08-18 16:37:33 -0400

    changes

---
+++ assets/dhhp/renderers/heroes/omniman_moustache.js
@@ -0,0 +1,17 @@
+extend("dhhp:omniman");
+loadTextures({
+    "layer1": "dhhp:other/omniman_stache_layer1",
+});
+
+function init(renderer) {
+    parent.init(renderer);
+
+    renderer.setItemIcons("omniman_0", "omniman_1", "omniman_2", "omniman_3");
+
+    renderer.showModel("CHESTPLATE", "head", "body", "rightArm", "leftArm", "rightLeg", "leftLeg");
+    renderer.fixHatLayer("CHESTPLATE");
+}
+
+function initEffects(renderer) {
+    parent.initEffects(renderer);
+}
