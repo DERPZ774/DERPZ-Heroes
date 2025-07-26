6180d5619857aab1ea0e8db7ce9d5ce6d9d9d55c
SkarMC <31092540+SkarMC@users.noreply.github.com>
2025-07-26 10:39:29 -0400

    omniman alt

---
+++ assets/dhhp/renderers/heroes/omniman_animated.js
@@ -0,0 +1,6 @@
+extend("dhhp:omniman");
+loadTextures({
+    "layer1": "dhhp:omniman/omniman_animated_layer1",
+    "layer2": "dhhp:omniman/omniman_animated_layer2",
+    "cape": "dhhp:omniman/omniman_animated_cape"
+});
